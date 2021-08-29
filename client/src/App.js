857d099ad1cd632c2275cf3772bb5324f3fb1fba
Your Name <jasjohn3@uat.edu>
2021-08-28 22:46:00 -0700

    Initial Page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
-
 import './App.css';
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar bg='success' sticky='top' variant='dark'>
+          <Container>
+            <Navbar.Brand href='#home'>📝Note Taker</Navbar.Brand>
+            <Switch>
+              <Nav className='me-auto'>
+                <Nav.Link href='#home'>Home</Nav.Link>
+                <Nav.Link href='/notes'>Notes</Nav.Link>
+              </Nav>
+            </Switch>
+          </Container>
+        </Navbar>
+        <Container>
+          <div className='jumbotron text-center'>
+            <h1 className='display-4'>
+              Note Taker{' '}
+              <span role='img' aria-label='Memo'>
+                📝
+              </span>
+            </h1>
+            <h4 className='mt-4'>Take notes with Express</h4>
+            <a className='btn btn-dark btn-lg mt-4' href='/notes' role='button'>
+              Get Started
+            </a>
+          </div>
+        </Container>
+      </div>
+    </Router>
   );
 }
 
