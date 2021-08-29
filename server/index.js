60454f858c40992cbbde99a1ddbecdd1c139bc6b
Your Name <jasjohn3@uat.edu>
2021-08-28 21:51:22 -0700

    React setup

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 PORT = process.env.PORT || 5001;
-console.log(process.env.PORT);
+const password = process.env.MYSQL_PASSWORD;
+const cors=require('cors');
+app.use(cors());
+app.use(express.json());
+
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 });
